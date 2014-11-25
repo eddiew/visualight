af4b3bfd03edccb315198b13b1f5e5a37c54c3d3
EddieW <eddie.wang@frontier.com>
2014-11-24 21:29:12 -0800

    color output to arduino

--- ArduinoControl/ArduinoControl/Program.cs
+++ ArduinoControl/ArduinoControl/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Numerics;
@@ -20,6 +19,7 @@ namespace ArduinoControl
         const int BufferSamples = 4096;
         const int SampleSize = 4; // 2 shorts per sample
         const int BufferSize = BufferSamples * SampleSize;
+        const int BRIGHTNESS_FACTOR = 1;
 
         static void Main(string[] args)
         {
@@ -69,13 +69,18 @@ namespace ArduinoControl
 
         static void Visualize(Complex[] fourierOutput, ArduinoControl arduino)
         {
-            Color vizColor = new Color();
+            Color vizColor = new Color(0, 0, 0);
             for (int i = 0; i < fourierOutput.Length / 2; i++) // divide by 2 b/c nyquist limit
             {
                 float freq = 44100 * i / fourierOutput.Length;
-                double power = fourierOutput[i].Real * fourierOutput[i].Real + fourierOutput[i].Imaginary * fourierOutput[i].Imaginary;
-                float note = midiNote(freq) % 12;
+                float power = (float)(fourierOutput[i].Real * fourierOutput[i].Real + fourierOutput[i].Imaginary * fourierOutput[i].Imaginary);
+                float note = midiNote(freq);
+                float h = note / 12;
+                float s = 1;
+                float l = power * BRIGHTNESS_FACTOR / (32768 * fourierOutput.Length);
+                AddColors(vizColor, ColorFromHSL(h, s, l));
             }
+            arduino.sendCommand(String.Format("r:{0} g:{1} b:{2}", vizColor.r, vizColor.g, vizColor.b));
         }
 
         static float midiNote(float frequency)
@@ -83,9 +88,69 @@ namespace ArduinoControl
             return (float)(69 + 12 * Math.Log(frequency / 440, 2));
         }
 
-        static Color ColorFromHSL()
+        struct Color
         {
+            public float r, g, b;
+            public Color(float r, float g, float b)
+            {
+                this.r = r;
+                this.g = g;
+                this.b = b;
+            }
+        }
 
+        static Color ColorFromHSL(float h, float s, float l)
+        {
+            float r = 0, g = 0, b = 0;
+            float temp1, temp2;
+
+            if (l == 0)
+            {
+                r = g = b = 0;
+            }
+            else
+            {
+                if (s == 0)
+                {
+                    r = g = b = l;
+                }
+                else
+                {
+                    temp2 = ((l <= 0.5f) ? l * (1.0f + s) : l + s - (l * s));
+                    temp1 = 2.0f * l - temp2;
+
+                    float[] t3 = new float[] { h + 1.0f / 3.0f, h, h - 1.0f / 3.0f };
+                    float[] clr = new float[] { 0, 0, 0 };
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (t3[i] < 0)
+                            t3[i] += 1.0f;
+                        if (t3[i] > 1)
+                            t3[i] -= 1.0f;
+
+                        if (6.0f * t3[i] < 1.0f)
+                            clr[i] = temp1 + (temp2 - temp1) * t3[i] * 6.0f;
+                        else if (2.0f * t3[i] < 1.0f)
+                            clr[i] = temp2;
+                        else if (3.0f * t3[i] < 2.0f)
+                            clr[i] = (temp1 + (temp2 - temp1) * ((2.0f / 3.0f) - t3[i]) * 6.0f);
+                        else
+                            clr[i] = temp1;
+                    }
+                    r = clr[0];
+                    g = clr[1];
+                    b = clr[2];
+                }
+            }
+
+            return new Color(255 * r, 255 * g, 255 * b);
+        }
+
+        static void AddColors(Color a, Color b)
+        {
+            a.r += b.r;
+            a.g += b.g;
+            a.b += b.b;
         }
     }
 }
