b54b7039f570a8f57b71a188602e4e259ab9e7fc
EddieW <eddiew@stanford.edu>
2014-11-24 19:02:39 -0800

    write to buffer

--- ArduinoControl/ArduinoControl/Program.cs
+++ ArduinoControl/ArduinoControl/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CSCore;
+using CSCore.Codecs.WAV;
 using CSCore.SoundIn;
 using CSCore.Streams;
 using CSCore.DSP;
@@ -12,18 +14,14 @@ namespace ArduinoControl
 {
     class Program
     {
-        private WaveInCaps selectedDevice;
-        private WaveIn waveIn;
+        const int BufferSize = 44100 * sizeof(short) * 2 / 10;
+        static short[] buffer = new short[BufferSize];
+        static int ReadOffset = 0;
 
         static void Main(string[] args)
         {
             ArduinoControl arduino = new ArduinoControl("COM3");
-            AudioRecorder recorder = new AudioRecorder();
-            foreach (var device in WaveIn.Devices)
-            {
-                Console.WriteLine(device.Name);
-            }
-            recorder.record();
+
             //while(true)
             //{
             //    string s = Console.ReadLine();
@@ -33,6 +31,35 @@ namespace ArduinoControl
             //    }
             //    arduino.sendCommand(s);
             //}
+            using (WasapiCapture capture = new WasapiCapture(false, CSCore.CoreAudioAPI.AudioClientShareMode.Exclusive, 0))
+            {
+                capture.Initialize();
+                //setup an eventhandler to receive the recorded data
+                capture.DataAvailable += (s, e) =>
+                {
+                    int bytesToWrite = Math.Min(e.ByteCount, BufferSize - ReadOffset);
+                    //save the recorded audio
+                    w.Write(e.Data, e.Offset, bytesToWrite);
+                    w.Write(e.Data.Skip(bytesToWrite).ToArray(), 0, e.ByteCount - bytesToWrite);
+                    int newReadOffset = (ReadOffset + e.ByteCount) % BufferSize;
+                    if ((newReadOffset >= BufferSize / 2 && ReadOffset < BufferSize / 2) ||
+                        (newReadOffset < BufferSize / 2 && ReadOffset >= BufferSize / 2))
+                    {
+
+                    }
+                    ReadOffset = newReadOffset;
+                };
+
+                //start recording
+                capture.Start();
+
+                Console.ReadKey();
+
+                //stop recording
+                capture.Stop();
+            }
         }
+
+        void fourierTransform(MemoryStream )
     }
 }
